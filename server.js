a6b034bd3138c858aba17d9bfd41a1ac3eb7a0d9
TravisMaloney <travis.maloney1@gmail.com>
2017-03-05 11:57:28 -0500

    server config

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express')
 var app = express()
+var router = express.Router();
+
 
 app.set('port', (process.env.PORT || 80))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.render('index.html')
+router.get('/', function(request, response) {
+  response.render('index')
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+module.express = router;
