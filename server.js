d12e412192aa2b7ede8f313e15b8bff622a63633
TravisMaloney <travis.maloney1@gmail.com>
2017-03-05 11:50:56 -0500

    configuring server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 80))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.render('index')
 })
 
 app.listen(app.get('port'), function() {
