d4e6b65b005eb4e589c379e9618f2a5d0ba03718
TravisMaloney <travis.maloney1@gmail.com>
2017-03-05 21:26:19 -0500

    mail form

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-var engines = require('consolidate');
+const engines = require('consolidate');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
 
 app.engine('html', engines.mustache);
 app.set('view engine', 'html');
@@ -17,8 +21,36 @@ app.get("/", function(req,res){
     res.render('index.html')
 });
 
-app.listen(80, () => {
+app.listen(8080, () => {
 	console.log('Server Started on http://localhost:8080');
 	console.log('Press CTRL + C to stop server');
 });
 
+app.get('/contact', function (req, res) {
+  var mailOpts, smtpTrans;
+  //Setup Nodemailer transport, I chose gmail. Create an application-specific password to avoid problems.
+  smtpTrans = nodemailer.createTransport({
+      service: 'Gmail',
+      host: "smtp.gmail.com",
+      auth: {
+          user: "maloney.travis1@gmail.com",
+          pass: ''
+      }
+  });
+  //Mail options
+  mailOpts = {
+      to: 'travis.maloney1@gmail.com',
+      subject: req.query.subject,
+      text: req.query.contentmessage + ' .......from ' + req.query.name + ' at ' + req.query.from
+  };
+
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+      if(error){
+            console.log(error);
+        res.end('');
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end('');
+         }
+  });
+});
